558cf28dec4c3dfbb11824ee91a998f50d1c0d4a
chandan <chandan868@gmail.com>
2024-02-19 00:24:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  
+  
+  return (
+    <>
+      <nav>
+      <a href="#" className='one'>Divorced Person Concern Advocate</a>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact Us</a>
+      </nav>
+      <header>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        copyright reserves,2024
+      </footer>
+    </>
+  )
+}
+
+export default App
