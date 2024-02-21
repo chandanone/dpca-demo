e804998db45630dc6b22c904017a01a9ca484947
chandan <chandan868@gmail.com>
2024-02-21 15:15:59 +0530

    updated navbar and header

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,37 @@ function App() {
   
   return (
     <>
-      <nav>
-      <a href="#" className='logo'>DPCA</a>
-        <a href="#" className='home'>Home</a>
-        <a href="#" className='about'>About</a>
-        <a href="#" className='contact'>Contact Us</a>
-      </nav>
       <header>
+        <div className='logo'>LOGO- DPCA</div>
+        <div className='timing'>
+          <div>HOURS: MONDAY - FRIDAY; SATURDAY;</div>
+          <div>9.00am - 6.00pm; 11am-4pm;</div>
+          <div>Jail/Police Department visits: 24/7</div>
+        </div>
+        <div className='contact'>
+          <div><a href="">JOE@DPCALAWGROUP.COM</a></div>
+          <div>(+91) 00000-00000</div>
+          <div>English\Hindi</div>
+        </div>
       </header>
+      <nav>
+        <div className='nav'>
+          
+          <div className='nav-1'>
+             <a href="#" className='home'>HOME</a>
+             <a href="#" className='areas'>PRACTICE AREAS</a>
+             <a href="#" className='about'>ABOUT US</a>
+             <a href="#" className='blog'>BLOG</a>
+             <a href="#" className='case'>CASE VICTORIES</a>
+             <a href="#" className='contact'>CONTACT</a>
+           
+          </div>
+          <div className='nav-2'>
+            <a href="#" className='consultation'><button id='consult'>Free Consultation 24/7</button></a>
+          </div>
+        </div>
+      </nav>
+      
       <main>
 
       </main>
